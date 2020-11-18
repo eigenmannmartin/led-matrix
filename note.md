c1b299e5fd202f78b50a6dcab43d6909f17eca48
Martin Eigenmann <martin@pop-os.localdomain>
2020-11-18 11:06:40 +0100

    feat() adds flask server

---
+++ note.md
@@ -0,0 +1,25 @@
+[
+[[255,255,125], [125,0,125]],
+[[255,255,125], [125,0,125]],
+[[255,255,125], [125,0,0]],
+[[0,255,0], [0,0,255], [255,0,0]],
+[[255,255,0], [0,255,255], [255,0, 255]]
+]
+
+ledtest/1
+
+http://www.hivemq.com/demos/websocket-client/
+
+
+
+sudo apt-get install -y python3 git python3-pip
+sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel paho-mqtt
+
+export FLASK_APP=server.py
+export FLASK_ENV=development
+python -m flask run
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+import paho.mqtt.client as mqtt
+app = Flask(__name__)
+
+
+def get_empty_display():
+    return [
+        [[255, 255, 255] for y in range(32)] for x in range(8)
+    ]
+
+
+DISPLAY = get_empty_display()
+
+MQTT_TOPIC = "ledtest/1"
+MQTT_HOST = "broker.mqttdashboard.com"
+MQTT_PORT = 1883
+MQTT_TIMEOUT = 60
+client = mqtt.Client()
+client.connect(MQTT_HOST, MQTT_PORT, MQTT_TIMEOUT)
+client.loop_start()
+
+
+@app.route('/')
+def index():
+    global DISPLAY
+    return jsonify(DISPLAY)
+
+
+@app.route('/set',  methods=['GET', 'POST'])
+def set():
+    global DISPLAY
+    req_data = request.get_json()
+    DISPLAY = get_empty_display()
+
+    if len(req_data):
+
+        for entry in req_data:
+            x = entry['x']
+            y = entry['y']
+            c = entry['c']
+
+            DISPLAY[y][x] = c
+    else:
+        DISPLAY = get_empty_display()
+
+    client.publish(MQTT_TOPIC, str(DISPLAY))
+    return "ok"
